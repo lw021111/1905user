8274355e4610b26060c5f1c80b405f7f97e434cf
lw021111 <2841732294@qq.com>
2020-01-12 14:01:55 +0800

    user

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Model\CommonModel;
 class TestController extends Controller{
+	function reg(){
+		$reg_info = [
+			'user_name' => 'liuwei1',
+			'user_email' => 'liuwei1@qq.com',
+			'pass1' => '11111',
+			'pass2' => '11111',
+		];
 
+		//请求admin 注册接口
+		$url="http://admin.com/reg";
+		$response=CommonModel::curlPost($url,$reg_info);
+		echo '<pre>';print_r($response);echo '</pre>';
+	}
+	function login(){
+		$login_info = [
+			'user_name'=>'liuwei1',
+			'user_pwd' =>'11111',
+		];
 
+		//请求admin 登陆接口
+		$url='http://admin.com/login';
+		$response=CommonModel::curlPost($url,$login_info);
+		echo '<pre>';print_r($response);echo '</pre>';
+	}    
    
-
+	function getData(){
+		$token='9607d89acef20d2419ef';
+		$user_id=10;
+		//请求admin 获取数据接口
+		$url='http://admin.com/time';
+		$header = [
+			'token:'.$token,
+			'uid:'.$user_id
+		];
+		$response=CommonModel::curlGet($url,$header);
+		echo '<pre>';print_r($response);echo '</pre>';
+	}
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,7 @@ Route::get('/info', function () {
     phpinfo();
 });
 
-Route::get('/req','TestController@req');
-Route::get('/encrypt','TestController@encrypt');
-Route::get('/encrypt2','TestController@encrypt2');
+Route::any('/reg','TestController@reg');
+Route::any('/login','TestController@login');
+Route::any('/getdata','TestController@getData');
 
-Route::get('/rsa1','TestController@rsa1');
-
-//curl测试
-Route::get('/curl1','TestController@curl1');
-Route::any('/curl2','TestController@curl2');
-Route::get('/curl3','TestController@curl3');
-Route::get('/curl4','TestController@curl4');
-
-//签名
-Route::get('/sign1','TestController@sign1');
-Route::get('/sign2','TestController@sign2');
