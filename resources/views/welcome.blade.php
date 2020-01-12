1051df81049c371cef271e1b16d5cf57f287fbdc
lw021111 <2841732294@qq.com>
2020-01-12 09:39:02 +0800

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    client
+                    1905user
                 </div>
 
                 <div class="links">
